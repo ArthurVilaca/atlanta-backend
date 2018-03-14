0016fe1207193c06bc938adb44eeb1bcae64393e
arthurvsn <arthurvsn@gmail.com>
2018-03-14 12:10:45 -0300

    opcoes de banco de componentes e documentacao

---
+++ database/migrations/2018_03_14_142800_create_config_components_table.php
@@ -0,0 +1,48 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateConfigComponentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('config_components', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->string('text1')->nullable();
+            $table->string('text2')->nullable();
+            $table->string('text3')->nullable();
+            $table->string('text4')->nullable();
+            $table->string('text5')->nullable();
+            $table->string('image1')->nullable();
+            $table->string('image2')->nullable();
+            $table->string('image3')->nullable();
+            $table->string('background_color')->nullable();
+            $table->string('min_height')->nullable();
+            $table->boolean('can_edit_background_image')->nullable();
+            $table->boolean('can_edit_background_color')->nullable();
+
+            $table->integer('component_id')->unsigned();
+            $table->foreign('component_id')->references('id')->on('components');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('config_components');
+    }
+}
---
+++ database/migrations/2018_03_14_143354_create_pages_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePagesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('pages', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name')->nullable();
+            $table->string('url')->nullable();
+            $table->string('file')->nullable();
+            $table->enum('status', ['published', 'canceled'])->nullable();
+            $table->integer('client_id')->unsigned();
+            $table->foreign('client_id')->references('id')->on('clients');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('pages');
+    }
+}
