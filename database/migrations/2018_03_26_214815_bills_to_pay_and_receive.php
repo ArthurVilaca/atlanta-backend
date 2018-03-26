a6c0c6cc652cf268e7b5387858d33a6002feefb0
Arthur <vilaca.arthur@yahoo.com.br>
2018-03-26 19:05:47 -0300

    create the bills migration

---
+++ database/migrations/2018_03_26_214815_bills_to_pay_and_receive.php
@@ -0,0 +1,57 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class BillsToPayAndReceive extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('billspay', function (Blueprint $table) {
+            $table->increments('id');
+
+            $table->integer('dealer_id')->unsigned();
+            $table->foreign('dealer_id')->references('id')->on('dealers');
+
+            $table->double('amount');
+
+            $table->dateTime('issue_date');
+            $table->dateTime('due_date');
+            $table->dateTime('payment_date')->nullable();
+
+            $table->timestamps();
+        });
+
+        Schema::create('billsreceive', function (Blueprint $table) {
+            $table->increments('id');
+
+            $table->integer('client_id')->unsigned();
+            $table->foreign('client_id')->references('id')->on('clients');
+
+            $table->double('amount');
+
+            $table->dateTime('issue_date');
+            $table->dateTime('due_date');
+            $table->dateTime('payment_date')->nullable();
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('billspay');
+        Schema::dropIfExists('billsreceive');
+    }
+}
